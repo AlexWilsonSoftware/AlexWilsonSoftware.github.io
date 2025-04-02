ec2da90e2e47ce5313071371a6d379f57962eded
Alex Wilson <awi218@uclive.ac.nz>
2025-04-03 11:05:01 +1300

    Made it more basic

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ textAlign: "center", padding: "20px" }}>
+        <h1>I'm Alex Wilson and this is my website</h1>
+      </div>
   );
 }
 
